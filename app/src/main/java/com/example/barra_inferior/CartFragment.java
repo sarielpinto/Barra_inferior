4123a39a1f700df4dcb1889693b9d7e40614ae58
sarielpinto <48193176+sarielpinto@users.noreply.github.com>
2019-05-15 14:32:38 -0500

    Ultima_parte

---
+++ app/src/main/java/com/example/barra_inferior/CartFragment.java
@@ -0,0 +1,33 @@
+package com.example.barra_inferior;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CartFragment extends Fragment {
+
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+    public static CartFragment newInstance(String param1, String param2) {
+        CartFragment fragment = new CartFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
